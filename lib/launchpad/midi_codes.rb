7ccc96a51ea7b9b62ebb13b6dea16fc7ae20a8a2
Jon Frisby <jfrisby@mrjoy.com>
2015-07-26 02:53:28 -0700

    Whitespace cleanups.

--- lib/launchpad/midi_codes.rb
+++ lib/launchpad/midi_codes.rb
@@ -1,8 +1,8 @@
 module Launchpad
-  
+
   # Module defining constants for MIDI codes.
   module MidiCodes
-    
+
     # Module defining MIDI status codes.
     module Status
       NIL           = 0x00
@@ -11,7 +11,7 @@ module Launchpad
       MULTI         = 0x92
       CC            = 0xB0
     end
-    
+
     # Module defininig MIDI data 1 (note) codes for control buttons.
     module ControlButton
       UP            = 0x68
@@ -23,7 +23,7 @@ module Launchpad
       USER2         = 0x6E
       MIXER         = 0x6F
     end
-    
+
     # Module defininig MIDI data 1 (note) codes for scene buttons.
     module SceneButton
       SCENE1        = 0x08
@@ -35,18 +35,18 @@ module Launchpad
       SCENE7        = 0x68
       SCENE8        = 0x78
     end
-    
+
     # Module defining MIDI data 2 (velocity) codes.
     module Velocity
       TEST_LEDS     = 0x7C
     end
-    
+
     # Module defining MIDI data 2 codes for selecting the grid layout.
     module GridLayout
       XY            = 0x01
       DRUM_RACK     = 0x02
     end
-    
+
   end
-  
+
 end
