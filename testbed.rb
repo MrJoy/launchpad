20fccf2d4683f3d069a4e536d03a140902c44ad0
Jon Frisby <jfrisby@mrjoy.com>
2015-08-23 04:15:06 -0700

    Make this demo much more interesting.

--- testbed.rb
+++ testbed.rb
@@ -6,8 +6,21 @@ Bundler.require(:default, :development)
 
 require "launchpad"
 
+QUADRANTS = [
+  [{ red: 0x3F, green: 0x00, blue: 0x00 }, { red: 0x00, green: 0x3F, blue: 0x00 }],
+  [{ red: 0x00, green: 0x00, blue: 0x3F }, { red: 0x3F, green: 0x3F, blue: 0x00 }],
+]
+SCALE = 2
+
 def base_color(x, y)
-  { red: 0x00, green: (x * 2) + 0x10, blue: (y * 2) + 0x10 }
+  quad_x = x / 4
+  quad_y = 1 - (y / 4)
+  quad = QUADRANTS[quad_y][quad_x]
+  tmp = { red: 0x00 + quad[:red], green: (x * SCALE) + quad[:green], blue: (y * SCALE) + quad[:blue] }
+  tmp.keys.each do |key|
+    tmp[key] = 0x3F if tmp[key] > 0x3F
+  end
+  tmp
 end
 
 GRID = (0..7).map { |x| (0..7).map { |y| { grid: [x, y] } } }.flatten
@@ -37,7 +50,7 @@ interaction.response_to(:grid) do |inter, action|
   x = action[:x]
   y = action[:y]
   if action[:state] == :down
-    value = { red: 0x3F, green: 0x00, blue: 0x00 }
+    value = { red: 0x3F, green: 0x3F, blue: 0x3F }
   else
     value = base_color(x, y)
   end
@@ -45,10 +58,42 @@ interaction.response_to(:grid) do |inter, action|
   inter.device.change(value)
 end
 
+def flip_quad!(inter, cc, quad_x, quad_y)
+  quad                      = QUADRANTS[quad_y][quad_x]
+  QUADRANTS[quad_y][quad_x] = { red: 0x3F - quad[:red],
+                                green: 0x3F - quad[:green],
+                                blue: 0x3F - quad[:blue] }
+  FLIPPED[quad_y][quad_x]   = !FLIPPED[quad_y][quad_x]
+  if FLIPPED[quad_y][quad_x]
+    color = { red: 0x1F, green: 0x1F, blue: 0x1F }
+  else
+    color = { red: 0x03, green: 0x03, blue: 0x03 }
+  end
+  inter.device.change(color.merge(cc: cc))
+  init_board(inter)
+end
+
+CC = %i(up down left right session user1 user2 scene1 scene2 scene3 scene4 scene5 scene6 scene7 scene8)
+FLIPPED = [[false, false], [false, false]]
+interaction.response_to(:scene1, :down) do |inter, action|
+  flip_quad!(inter, action[:type], 0, 0)
+end
+interaction.response_to(:scene2, :down) do |inter, action|
+  flip_quad!(inter, action[:type], 1, 0)
+end
+interaction.response_to(:scene3, :down) do |inter, action|
+  flip_quad!(inter, action[:type], 0, 1)
+end
+interaction.response_to(:scene4, :down) do |inter, action|
+  flip_quad!(inter, action[:type], 1, 1)
+end
+
 interaction.response_to(:mixer, :down) do |_interaction, action|
-  puts "STAHP!"
   goodbye(interaction)
   interaction.stop
 end
+interaction.device.change({ red: 0x03, green: 0x00, blue: 0x00, cc: :mixer })
+interaction.device.changes(%i(scene1 scene2 scene3 scene4).map { |cc| { red: 0x03, green: 0x02, blue: 0x03, cc: cc } })
+
 init_board(interaction)
 interaction.start
