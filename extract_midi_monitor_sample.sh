2ed7c525f2824498cfebf39cfb155eed152d49a5
Jon Frisby <jfrisby@mrjoy.com>
2015-09-05 11:17:23 -0700

    Helper for reverse-engineering the Orbit.

---
+++ extract_midi_monitor_sample.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+FNAME=${1:-}
+
+if [[ $FNAME == "" ]]; then
+  echo "Usage: extract_midi_monitor_sample.sh <save_file.mmon>"
+  exit 1
+fi
+
+plutil -convert xml1 $FNAME
+
+xpath $FNAME "//dict/data" 2>/dev/null |
+  grep -v -E '(^<data>)|(</data>$)' |
+  base64 -D |
+  plutil -convert xml1 - -o - > ${FNAME%.mmon}.tmp
+
+# Not sure we're guaranteed to get the same number of elements every time,
+# so the `5` below may be brittle!
+xpath ${FNAME%.mmon}.tmp "//dict/array/dict[5]/data" 2>/dev/null |
+  grep -v -E '(^<data>)|(</data>$)' |
+  base64 -D > ${FNAME%.mmon}.raw
