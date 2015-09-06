47134b65b73e8a5a7c5e8c4ea8f64aabb8767796
Jon Frisby <jfrisby@mrjoy.com>
2015-09-06 11:37:26 -0700

    Tidy up.

--- launchpad_testbed.rb
+++ launchpad_testbed.rb
@@ -12,9 +12,6 @@ require "control_center"
 # Set row:  F0h 00h 20h 29h 02h 18h 0Dh <Row> <Colour> F7h
 # Set col:  F0h 00h 20h 29h 02h 18h 0Ch <Column> <Colour> F7h
 
-# By default, Launchpad MK2 will flash and pulse at 120 BPM. This can be altered by sending Launchpad MK2 F8h (248) messages (MIDI clock), which should be sent at a rate of 24 per beat. To set a tempo of 100 BPM, 2400 MIDI clock messages should be sent each minute, or with a time interval of 25ms.
-# Launchpad MK2 supports tempos between 40 and 240 BPM.
-
 # Configuration
 SCALE       = 2
 TIME_SCALE  = 4.0
@@ -88,10 +85,6 @@ interaction.response_to(:grid) do |inter, action|
 end
 
 def flip_quad!(inter, cc, quad_x, quad_y)
-  # quad                      = QUADRANTS[quad_y][quad_x]
-  # QUADRANTS[quad_y][quad_x] = { red:    0x3F - quad[:red],
-  #                               green:  0x3F - quad[:green],
-  #                               blue:   0x3F - quad[:blue] }
   FLIPPED[quad_y][quad_x]   = !FLIPPED[quad_y][quad_x]
   if FLIPPED[quad_y][quad_x]
     color = { red: 0x1F, green: 0x1F, blue: 0x1F }
@@ -100,15 +93,6 @@ def flip_quad!(inter, cc, quad_x, quad_y)
   end
   inter.device.change(color.merge(cc: cc))
 end
-
-
-# def shift_quad!(inter, quad_x, quad_y)
-#   quad                      = QUADRANTS[quad_y][quad_x]
-#   QUADRANTS[quad_y][quad_x] = { red:    (quad[:red]   + 0x0D) % 0x3F,
-#                                 green:  (quad[:green] + 0x11) % 0x3F,
-#                                 blue:   (quad[:blue]  + 0x1F) % 0x3F }
-#   init_board(inter)
-# end
 
 interaction.response_to(:scene1, :down) do |inter, action|
   flip_quad!(inter, action[:type], 0, 0)
