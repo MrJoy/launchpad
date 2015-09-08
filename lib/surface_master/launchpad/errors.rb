39dcf26433a8d16521ff976eeeaa1f9212817a7b
Jon Frisby <jfrisby@mrjoy.com>
2015-09-08 15:18:27 -0700

    Whoops, forgot to namespace this.

--- lib/surface_master/launchpad/errors.rb
+++ lib/surface_master/launchpad/errors.rb
@@ -1,8 +1,10 @@
-module Launchpad
-  # Generic launchpad error.
-  class LaunchpadError < SurfaceMaster::GenericError; end
+module SurfaceMaster
+  module Launchpad
+    # Generic launchpad error.
+    class LaunchpadError < SurfaceMaster::GenericError; end
 
-  # Error raised when <tt>x/y</tt> coordinates outside of the grid
-  # or none were specified.
-  class NoValidGridCoordinatesError < LaunchpadError; end
+    # Error raised when <tt>x/y</tt> coordinates outside of the grid
+    # or none were specified.
+    class NoValidGridCoordinatesError < LaunchpadError; end
+  end
 end
