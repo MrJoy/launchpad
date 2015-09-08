43a6a4752dfd5d6db76f7c2221d7358fbe16583b
Jon Frisby <jfrisby@mrjoy.com>
2015-09-08 14:43:08 -0700

    Cruft removal.

--- lib/surface_master/launchpad/errors.rb
+++ lib/surface_master/launchpad/errors.rb
@@ -5,7 +5,4 @@ module Launchpad
   # Error raised when <tt>x/y</tt> coordinates outside of the grid
   # or none were specified.
   class NoValidGridCoordinatesError < LaunchpadError; end
-
-  # Error raised when wrong brightness was specified.
-  class NoValidBrightnessError < LaunchpadError; end
 end
